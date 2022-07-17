d0a2e4c7407d8ae7d4635f76c2fa7b71125c2f32
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-17 14:33:31 +0500

    Create Practice03.js

---
+++ Practice03.js
@@ -0,0 +1,14 @@
+    <script>
+        /*
+Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
+
+        */
+       var year =parseInt(prompt("Enter an year"));
+       if(year%4 == 0){
+            alert(year + " is a leap year");
+       }else{
+        alert(year + " is not a leap year")
+       }
+
+
+    </script>
